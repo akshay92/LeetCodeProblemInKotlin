683f6a627ec53963ea49bf7e53e02d244ebcf39f
Akshay Trivedi <akshaytrivedi92@gmail.com>
2023-09-15 08:29:44 +0530

    RemoveDuplicatesFromSortedArray

---
+++ src/main/kotlin/easy/array/RemoveDuplicatesFromSortedArray.kt
@@ -0,0 +1,24 @@
+package easy.array
+
+/*
+Maintain var for unique count
+Run a loop
+   if unique == 0 || current item > previous unique item
+      update item array to current item
+
+
+ */
+class RemoveDuplicatesFromSortedArray {
+    fun removeDuplicates(nums: IntArray): Int {
+        var i:Int = 0
+
+        for(n in nums){
+            if(i==0 || n > nums[i-1]){
+                nums[i++] = n
+            }
+
+        }
+
+        return i
+    }
+}
