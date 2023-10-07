83b811f0f34faff3a9aec46c9bfe015b853946bc
Akshay Trivedi <akshaytrivedi92@gmail.com>
2023-10-07 15:32:59 +0530

    PascalTriangle

---
+++ src/main/kotlin/easy/array/PascalTriangle.kt
@@ -0,0 +1,27 @@
+package easy.array
+
+class PascalTriangle {
+
+    fun generate(numRows: Int): List<List<Int>> {
+
+         var result = mutableListOf<MutableList<Int>>()
+
+        for(i in 0 until numRows){
+
+            var item = mutableListOf<Int>()
+
+            for (j in 0..i){
+                if(j == 0 ){
+                    item.add(1)
+                }
+                else{
+                    var preList = result[i-1]
+                    item.add(preList[j-1] + if(j < preList.size) preList[j] else 0)
+                }
+            }
+            result.add(item)
+        }
+
+        return result
+    }
+}
