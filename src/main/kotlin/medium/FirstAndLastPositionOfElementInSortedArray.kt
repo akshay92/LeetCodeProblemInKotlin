ea1f3ff3eb65d23bf3a464489ade49ad08026f4a
Akshay Trivedi <akshaytrivedi92@gmail.com>
2023-10-07 15:31:23 +0530

    FirstAndLastPositionOfElementInSortedArray

---
+++ src/main/kotlin/medium/FirstAndLastPositionOfElementInSortedArray.kt
@@ -0,0 +1,25 @@
+package medium
+
+class FirstAndLastPositionOfElementInSortedArray {
+
+    fun searchRange(nums: IntArray, target: Int): IntArray {
+        var output: IntArray = IntArray(2)
+        var isFirstItemSet = false
+
+        nums.forEachIndexed { index, item ->
+
+            if(item == target){
+                if(!isFirstItemSet){
+                    isFirstItemSet= true
+                    output[0]=index
+                }
+                else {
+                    output[1] = index
+                }
+            }
+        }
+        
+        return output
+    }
+
+}
