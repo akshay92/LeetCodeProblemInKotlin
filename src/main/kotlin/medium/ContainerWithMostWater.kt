d6fcad13735871fc1c513b8259f658fc3325da2b
Akshay Trivedi <akshaytrivedi92@gmail.com>
2023-10-07 15:24:12 +0530

    ContainerWithMostWater

---
+++ src/main/kotlin/medium/ContainerWithMostWater.kt
@@ -0,0 +1,21 @@
+package medium
+
+class ContainerWithMostWater {
+
+    fun maxArea(height: IntArray): Int {
+
+        var max = 0
+        var lastMaxIndex = 0
+
+        for(i in 0 until  height.size){
+            if(i == 0 || height[lastMaxIndex] >= height[i]) {
+                max = Math.max(max, height[i] * (i - lastMaxIndex))
+            }
+            else{
+                lastMaxIndex = i
+            }
+        }
+
+        return max
+    }
+}
